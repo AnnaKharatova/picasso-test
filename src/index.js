16a390ab69e66c5283d389c448f4bf41e54a39c4
Anna Haratova <black-isida@yandex.ru>
2023-10-19 20:03:40 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import {store} from './store/store'
 import App from './App';
 
@@ -9,11 +9,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={window.location.pathname || ""}>
+  <HashRouter /* basename={window.location.pathname || ""} */ >
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 
 );
 
